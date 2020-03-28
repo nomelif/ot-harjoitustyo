113118cbcd6913e334600dcc8f6c6fb23f109a48
Théo Friberg <fsmnarmosta@posteo.net>
2020-03-28 14:01:51 +0200

    Introductory attempt to generate a heightmap

--- MapApp/src/main/java/map/Map.java
+++ MapApp/src/main/java/map/Map.java
@@ -1,5 +1,8 @@
 package map;
 
+import com.flowpowered.noise.module.source.Perlin;
+import java.awt.image.BufferedImage;
+
 public class Map{
     private double[] data;
     private int width;
@@ -16,4 +19,28 @@ public class Map{
     public int getHeight(){
         return this.data.length / this.width;
     }
+
+    public void makePerlin(){
+        Perlin p = new Perlin();
+        for(int y = 0; y < this.getHeight(); y++){
+            for(int x = 0; x < this.getWidth(); x++){
+                this.data[y*this.getWidth() + x] = p.getValue(this.getWidth() / ((double) x), this.getHeight() / ((double) y), 0) / 2 + 0.5;
+            }
+        }
+    }
+
+    public BufferedImage toBufferedImage(){
+        BufferedImage result = new BufferedImage(this.getWidth(), this.getHeight(), BufferedImage.TYPE_INT_RGB);
+        for(int y = 0; y < this.getHeight(); y++){
+            for(int x = 0; x < this.getWidth(); x++){
+                int darkness = ((int) (this.data[y*this.getWidth() + x] * 255));
+                int rgb = darkness;
+                rgb = (rgb << 8) + darkness;
+                rgb = (rgb << 8) + darkness;
+                result.setRGB(x, y, rgb);
+            }
+        }
+        return result;
+    }
+
 }
--- MapApp/src/main/java/ui/Main.java
+++ MapApp/src/main/java/ui/Main.java
@@ -1,9 +1,23 @@
 package ui;
 
 import map.Map;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.*;
 
 class Main{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Generating perlin noise");
+        Map m = new Map(512, 512);
+        m.makePerlin();
+        System.out.println("Generating BufferedImage");
+        BufferedImage b = m.toBufferedImage();
+        System.out.println("Writing BufferedImage to /tmp/test.png");
+        try{
+            ImageIO.write(b, "png", new File("/tmp/test.png"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
     }
 }
