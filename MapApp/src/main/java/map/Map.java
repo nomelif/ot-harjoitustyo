57fbdf8e1e41f38ebe881f6452b31aa24a511885
Théo Friberg <fsmnarmosta@posteo.net>
2020-03-28 16:53:04 +0200

    Fix perlin, add indexing

--- MapApp/src/main/java/map/Map.java
+++ MapApp/src/main/java/map/Map.java
@@ -12,6 +12,10 @@ public class Map{
         this.width = width;
     }
 
+    public double index(int x, int y){
+        return this.data[y*this.getWidth() + x];
+    }
+
     public int getWidth(){
         return this.width;
     }
@@ -22,11 +26,29 @@ public class Map{
 
     public void makePerlin(){
         Perlin p = new Perlin();
+
+        double min = 1;
+        double max = -1;
+
+        // Generate
+        
         for(int y = 0; y < this.getHeight(); y++){
             for(int x = 0; x < this.getWidth(); x++){
-                this.data[y*this.getWidth() + x] = p.getValue(this.getWidth() / ((double) x), this.getHeight() / ((double) y), 0) / 2 + 0.5;
+                this.data[y*this.getWidth() + x] = p.getValue(((double) x+1) / this.getWidth(), ((double) y+1) / this.getHeight(), 0);
+                max = Math.max(max, this.data[y*this.getWidth() + x]);
+                min = Math.min(min, this.data[y*this.getWidth() + x]);
             }
         }
+
+        for(int y = 0; y < this.getHeight(); y++){
+            for(int x = 0; x < this.getWidth(); x++){
+                this.data[y*this.getWidth() + x] -= min;
+                this.data[y*this.getWidth() + x] /= (max - min);
+            }
+        }
+
+        // Regularize
+
     }
 
     public BufferedImage toBufferedImage(){
