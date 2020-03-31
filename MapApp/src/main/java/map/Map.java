b5094a5fc46396636ba0a4226175bae8f729b166
Théo Friberg <fsmnarmosta@posteo.net>
2020-03-31 16:57:39 +0300

    Redesigned makePerlin
    - Now adds the effect to existing map instead of overwriting
    - Parametrize scale, influence and offset
    - Make use of seed parameter
    - Tweak drop influence. bestDelta / 50 is still stable but makes the
    simulation twice faster

--- MapApp/src/main/java/map/Map.java
+++ MapApp/src/main/java/map/Map.java
@@ -32,47 +32,82 @@ public class Map{
         return this.data.length / this.width;
     }
 
-    public void makePerlin(){
-        Perlin p = new Perlin();
+    public void makePerlin(double scale, double influence, double offset){
+
+        // The new perlin will be generated into a new array and then regularized
+
+        double[] newMap = new double[data.length];
 
         double min = 1;
         double max = -1;
 
-        // Generate
+        // Generation step
         
         for(int y = 0; y < this.getHeight(); y++){
             for(int x = 0; x < this.getWidth(); x++){
-                this.data[y*this.getWidth() + x] = p.getValue(((double) x+1) / this.getWidth(), ((double) y+1) / this.getHeight(), 0);
-                max = Math.max(max, this.data[y*this.getWidth() + x]);
-                min = Math.min(min, this.data[y*this.getWidth() + x]);
+                newMap[y*this.getWidth() + x] += p.getValue(scale*((double) x+1) / this.getWidth(), scale*((double) y+1) / this.getHeight(), 0);
+                max = Math.max(max, newMap[y*this.getWidth() + x]);
+                min = Math.min(min, newMap[y*this.getWidth() + x]);
             }
         }
+
+        // Regularization step (clamp to the range [offset, influence + offset])
 
         for(int y = 0; y < this.getHeight(); y++){
             for(int x = 0; x < this.getWidth(); x++){
-                this.data[y*this.getWidth() + x] -= min;
-                this.data[y*this.getWidth() + x] /= (max - min);
+                this.data[y*this.getWidth() + x] += offset + influence*(newMap[y*this.getWidth() + x] - min)/(max-min);
             }
         }
 
-        // Regularize
+    }
 
+    public void waterCutoff(double cutoff){
+
+        // Cut everything below cutoff, then move everything down by cutoff
+
+        for(int y = 0; y < this.getHeight(); y++){
+            for(int x = 0; x < this.getWidth(); x++){
+                if(this.data[y*this.getWidth() + x] < cutoff){
+                    this.data[y*this.getWidth() + x] = 0;
+                }else{
+                    this.data[y*this.getWidth() + x] -= cutoff;
+                }
+            }
+        }
     }
 
     public void doErosion(int drops, int iterations){
-        Random r = new Random();
+
+        // Track the specified amount of drops for the specified amount of iterations
+
         for(int d = 0; d < drops; d++){
+
+            // Start the drop from a random location
+
             int x = r.nextInt(this.getWidth());
             int y = r.nextInt(this.getHeight());
             for(int i = 0; i < iterations; i++){
+
+                // The ideal point for the drop to continue to is (bestDx, bestDy)
+                // (= the point where the local gradient leads)
+
                 int bestDx = -1;
                 int bestDy = -1;
-                double bestDelta = 1;
+                double bestDelta = 1; // Measure of local gradient
                 for(int dx = -1; dx <= 1; dx++){
                     for(int dy = -1; dy <= 1; dy++){
-                        if(dx == 0 && dy == 0) continue;
+                        if(dx == 0 && dy == 0) continue; // Discount (x, y)
+
+                        // Discount locations that are off the map
+
                         if(x+dx < 0 || x + dx > this.getWidth()-1 || y+dy < 0 || y + dy > this.getHeight()-1) continue;
+
+                        // Compute the gradient (compensate for diagonal points being further away)
+
                         double delta = (data[(y+dy)*this.getWidth() + x+dx] - data[y*this.getWidth() + x]) / Math.sqrt(dx*dx + dy*dy);
+
+                        // Update info on the optimal path
+
                         if(bestDelta > delta){
                             bestDelta = delta;
                             bestDx = dx;
@@ -80,11 +115,14 @@ public class Map{
                         }
                     }
                 }
-                if(bestDelta > 0) break;
-                data[y*this.getWidth() + x] += bestDelta / 2;
+                if(bestDelta > 0) break; // If there is no way down, the drop dies
+
+                // bestDelta / 50 was found to be a good value experimentally
+
+                data[y*this.getWidth() + x] += bestDelta / 50;
                 y += bestDy;
                 x += bestDx;
-                data[y*this.getWidth() + x] -= bestDelta / 2;
+                data[y*this.getWidth() + x] -= bestDelta / 50;
             }
         }
     }
