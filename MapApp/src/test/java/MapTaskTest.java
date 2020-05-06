afed0380be4d91c3d4e5c2d8068ae4412e4fdbb9
Théo Friberg <fsmnarmosta@posteo.net>
2020-05-06 13:44:11 +0300

    Small test runtime optimisation

--- MapApp/src/test/java/MapTaskTest.java
+++ MapApp/src/test/java/MapTaskTest.java
@@ -35,8 +35,8 @@ public class MapTaskTest {
     
     @Test
     public void testMapSize() {
-        for(int width = 12; width <= 512; width+=20){
-            MapTask task = new MapTask(width, new OptionCollection("1337", 10, 0.5, 1, 0.2, 0));
+        for(int width = 12; width <= 64; width+=8){
+            MapTask task = new MapTask(width, new OptionCollection("1337", 10, 0.5, 1, 0.2, 5));
             task.enableTestingMode();
             Map m = task.call();
             assertEquals(m.getWidth(), width);
@@ -53,17 +53,17 @@ public class MapTaskTest {
         double[] mountainCutoffs = new double[]{0.8695450019790698, 0.78200580481437, 0.1804670943875072, 0.10396829449836575, 0.5466415102645301, 0.22963171913770175, 0.2583408273942259, 0.7519657374656038, 0.4191374850390841, 0.7165152542148352};
         double[] largeFeatureScales = new double[]{0.556970429887087, 0.7984905939123088, 0.28760598901932977, 0.33209089632821254, 0.4206201139503686, 0.21516104356464683, 0.6100791894282644, 0.7841532854081026, 0.30688093808484584, 0.6448030732731151};
         double[] seaCutoffs = new double[]{0.9032700108609004, 0.27376892023337607, 0.3303991753395543, 0.23378238020847597, 0.33967970769212064, 0.03983470939518119, 0.2653488142589967, 0.6148460939911095, 0.18566275577595526, 0.9431413912795532};
-        int[] erosionIterations = new int[]{3, 7, 8, 9, 3, 2, 7, 9, 5, 3};
+        int[] erosionIterations = new int[]{3, 1, 2, 4, 3, 2, 0, 1, 0, 3};
         for(int i = 0; i < 10; i++){
-            OptionCollection options = new OptionCollection(seeds[i], mountainScales[i], mountainCutoffs[i], largeFeatureScales[i], seaCutoffs[i], 0);
-			MapTask task1 = new MapTask(30, options);
+            OptionCollection options = new OptionCollection(seeds[i], mountainScales[i], mountainCutoffs[i], largeFeatureScales[i], seaCutoffs[i], erosionIterations[i]);
+			MapTask task1 = new MapTask(16, options);
 			task1.enableTestingMode();
 			Map map1 = task1.call();
-			MapTask task2 = new MapTask(30, options);
+			MapTask task2 = new MapTask(16, options);
 			task2.enableTestingMode();
 			Map map2 = task2.call();
-			for(int x = 0; x < 30; x++){
-				for(int y = 0; y < 30; y++){
+			for(int x = 0; x < 16; x++){
+				for(int y = 0; y < 16; y++){
 					assertEquals(map1.index(x, y), map2.index(x, y), 0);
 				}
 			}
